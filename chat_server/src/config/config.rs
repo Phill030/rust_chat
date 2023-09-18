5a127cef54b5908cf894746c69d7d709e53e6f2f
Phill <philltv.gaming@gmail.com>
2023-09-19 00:28:10 +0200

    feat(server): add config system instead of arguments

---
+++ chat_server/src/config/config.rs
@@ -0,0 +1,48 @@
+use crate::{error::ConfigError, types::Config};
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncWriteExt},
+};
+
+const CONFIG_NAME: &str = "config.toml";
+pub struct ConfigManager;
+
+impl ConfigManager {
+    pub async fn initialize_or_create() -> Result<Config, ConfigError> {
+        let file = File::open(CONFIG_NAME).await;
+
+        if file.is_err() {
+            let config = Config::default();
+
+            match File::create(CONFIG_NAME).await {
+                Ok(mut f) => {
+                    let pretty_config = toml::to_string_pretty(&config)?;
+                    if f.write_all(pretty_config.as_bytes()).await.is_err() {
+                        log::error!("Unable to create {CONFIG_NAME}!");
+                    }
+                }
+                Err(why) => {
+                    log::error!("Unable to create {CONFIG_NAME}! {why}");
+                }
+            }
+
+            return Ok(config);
+        } else {
+            let mut contents = vec![];
+            file.unwrap().read_to_end(&mut contents).await?;
+
+            let config: Result<Config, toml::de::Error> =
+                toml::from_str(std::str::from_utf8(&contents).unwrap());
+
+            let config = match config {
+                Ok(conf) => conf,
+                Err(why) => {
+                    log::error!("Unable to convert config to struct (Using default)! {why}");
+                    Config::default()
+                }
+            };
+
+            Ok(config)
+        }
+    }
+}
