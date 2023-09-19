191a679914320fdf053191c406cbafcb8617e06e
Phill <50775241+Phill030@users.noreply.github.com>
2023-09-19 11:45:31 +0200

    feat: add Client config

--- chat_client/src/types.rs
+++ chat_client/src/types.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub enum ServerProtocol {
     BroadcastMessage { sender: String, content: String },
@@ -9,4 +11,21 @@ pub enum ClientProtocol {
     SendMessage { hwid: String, content: String },
     ChangeUsername { hwid: String, new_username: String },
     RequestAuthentication { hwid: String },
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct Config {
+    pub endpoint: SocketAddr,
+    pub buffer_size: usize,
+    pub name: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            endpoint: "0.0.0.0:7878".parse().unwrap(),
+            buffer_size: 2048,
+            name: format!("User{}", rand::prelude::random::<i16>()),
+        }
+    }
 }
