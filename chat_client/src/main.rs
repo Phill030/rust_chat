60c5e80f43a1308aff1e0ca1c6c016ae941ed1c0
Phill <50775241+Phill030@users.noreply.github.com>
2023-09-20 08:57:46 +0200

    ref: use Struct & construct hwid only once

--- chat_client/src/main.rs
+++ chat_client/src/main.rs
@@ -15,96 +15,105 @@ static KEY: &str = "1234567890";
 #[tokio::main]
 async fn main() -> io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let hwid = construct_hwid();
 
     let config = ConfigManager::initialize_or_create().await.unwrap();
-    let stream = TcpStream::connect(config.endpoint)?;
+    if let Ok(stream) = TcpStream::connect(config.endpoint) {
+        log::info!("Connected to server");
+        Client::new(&stream, &config, &hwid)?;
+    }
 
-    log::info!("Connected to server");
+    Ok(())
+}
 
-    // It is required to send the HWID to the server to authorize with it
-    request_authentication(&stream);
+struct Client;
+impl Client {
+    fn new(stream: &TcpStream, config: &Config, hwid: &String) -> io::Result<()> {
+        // It is required to send the HWID to the server to authorize with it
+        Self::request_authentication(&stream);
 
-    let read_stream = stream.try_clone()?;
-    let cloned_config = config.clone();
-    thread::spawn(move || {
-        read_messages(read_stream, &cloned_config);
-    });
+        let read_stream = stream.try_clone()?;
+        let cloned_config = config.clone();
+        thread::spawn(move || {
+            Self::read_messages(read_stream, &cloned_config);
+        });
 
-    loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)?;
 
-        let trimmed_input = input.trim();
-        if trimmed_input.is_empty() {
-            continue;
+            let trimmed_input = input.trim();
+            if trimmed_input.is_empty() {
+                continue;
+            }
+
+            let message = ClientProtocol::SendMessage {
+                hwid: hwid.clone(), //TODO: replace
+                content: trimmed_input.to_owned(),
+            };
+
+            write_to_stream(&stream, &message);
         }
+    }
 
-        let message = ClientProtocol::SendMessage {
-            hwid: construct_hwid(), //TODO: replace
-            content: trimmed_input.to_owned(),
+    fn read_messages(mut stream: TcpStream, config: &Config) {
+        let mut buffer = vec![0; config.buffer_size];
+
+        loop {
+            match stream.read(&mut buffer) {
+                Ok(bytes_read) => {
+                    if bytes_read == 0 {
+                        log::warn!("Server disconnected");
+                        break;
+                    }
+
+                    let message = String::from_utf8_lossy(&buffer[0..bytes_read]).to_string();
+
+                    let deserialized_message: Result<ServerProtocol, _> =
+                        serde_json::from_str(&message);
+
+                    match deserialized_message {
+                        Ok(server_message) => match server_message {
+                            ServerProtocol::BroadcastMessage { sender, content } => {
+                                log::info!("Received '{content}' from {sender}");
+                            }
+                            ServerProtocol::AuthenticateToken { token } => {
+                                log::info!("Session-Token: {}", token);
+                            }
+
+                            // Every other message
+                            _ => {
+                                log::warn!(
+                                    "Received unknown message from server: {:#?}",
+                                    server_message
+                                );
+                            }
+                        },
+                        Err(_) => {
+                            log::warn!("Error parsing client message");
+                        }
+                    }
+
+                    // Clear the buffer
+                    buffer = vec![0; config.buffer_size];
+                }
+                Err(why) => {
+                    println!("Error reading from server! {why}");
+                    process::exit(0);
+                }
+            }
+        }
+    }
+
+    fn request_authentication(stream: &TcpStream) {
+        let message = ClientProtocol::RequestAuthentication {
+            hwid: construct_hwid(),
         };
 
-        write_to_stream(&stream, &message);
-    }
-}
-
-fn read_messages(mut stream: TcpStream, config: &Config) {
-    let mut buffer = vec![0; config.buffer_size];
-
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(bytes_read) => {
-                if bytes_read == 0 {
-                    log::warn!("Server disconnected");
-                    break;
-                }
-
-                let message = String::from_utf8_lossy(&buffer[0..bytes_read]).to_string();
-
-                let deserialized_message: Result<ServerProtocol, _> =
-                    serde_json::from_str(&message);
-
-                match deserialized_message {
-                    Ok(server_message) => match server_message {
-                        ServerProtocol::BroadcastMessage { sender, content } => {
-                            log::info!("Received message {content} from {sender}");
-                        }
-                        ServerProtocol::AuthenticateToken { token } => {
-                            log::info!("Session-Token: {}", token);
-                        }
-
-                        // Every other message
-                        _ => {
-                            log::warn!(
-                                "Received unknown message from server: {:#?}",
-                                server_message
-                            );
-                        }
-                    },
-                    Err(_) => {
-                        log::warn!("Error parsing client message");
-                    }
-                }
-
-                // Clear the buffer
-                buffer = vec![0; config.buffer_size];
-            }
-            Err(why) => {
-                println!("Error reading from server! {why}");
-                process::exit(0);
-            }
+        if !write_to_stream(stream, &message) {
+            log::error!("Error authenticating");
+            process::exit(0);
         }
-    }
-}
-
-fn request_authentication(stream: &TcpStream) {
-    let message = ClientProtocol::RequestAuthentication {
-        hwid: construct_hwid(),
-    };
-
-    if !write_to_stream(stream, &message) {
-        log::error!("Error authenticating");
-        process::exit(0);
     }
 }
 
@@ -117,7 +126,7 @@ fn construct_hwid() -> String {
     match builder.build(KEY) {
         Ok(k) => k,
         Err(why) => {
-            log::error!("Cannot construct HWID, {why}");
+            log::error!("Unable to construct HWID, {why}");
             process::exit(0);
         }
     }
