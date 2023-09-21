2db5d46014f355c3457d2f480c213b7c0c2d9f2a
Phill <50775241+Phill030@users.noreply.github.com>
2023-09-21 11:14:38 +0200

    ref: make protocols use string slice with lifetime

--- chat_server/src/types.rs
+++ chat_server/src/types.rs
@@ -1,26 +1,38 @@
 use std::net::SocketAddr;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
-pub enum ServerProtocol {
-    BroadcastMessage { sender: String, content: String },
-    AuthenticateToken { token: String },
+pub enum ServerProtocol<'a> {
+    BroadcastMessage { sender: &'a str, content: &'a str },
+    AuthenticateToken { token: &'a str },
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
-pub enum ClientProtocol {
-    SendMessage { hwid: String, content: String },
-    ChangeUsername { hwid: String, new_username: String },
-    RequestAuthentication { hwid: String },
+pub enum ClientProtocol<'a> {
+    SendMessage {
+        hwid: &'a str,
+        content: &'a str,
+    },
+    ChangeUsername {
+        hwid: &'a str,
+        new_username: &'a str,
+    },
+    RequestAuthentication {
+        hwid: &'a str,
+        name: &'a str,
+    },
+    HeartBeat {
+        hwid: &'a str,
+    },
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Clone, Eq, Hash)]
 pub struct Client {
     /// A custom name of the client
-    pub name: Option<String>,
-    // pub hwid: String,
+    pub name: String,
+    pub hwid: String,
     /// A Session-Token is a randomly generated String which changes on every reconnect.
     /// It can be used to validate the session of a client.
-    pub session_token: Option<String>,
+    pub session_token: String,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
