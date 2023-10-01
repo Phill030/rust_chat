9abd5cb7754a8c790f23a1b158986f7e65cd0144
Phill <philltv.gaming@gmail.com>
2023-10-01 02:38:06 +0200

    feat: add WriteToStreamError

--- chat_server/src/error.rs
+++ chat_server/src/error.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::Debug};
+
 #[derive(thiserror::Error, Debug)]
 pub enum ConfigError {
     #[error("Unable to access file!")]
@@ -5,4 +7,20 @@ pub enum ConfigError {
 
     #[error("Unable to convert config to string!")]
     TOML(#[from] toml::ser::Error),
+}
+
+#[derive(thiserror::Error)]
+pub enum WriteToStreamError {
+    #[error("Unable to write to stream")]
+    IO(#[from] std::io::Error),
+}
+
+impl Debug for WriteToStreamError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self)?;
+        if let Some(source) = self.source() {
+            writeln!(f, "Caused by:\n\t{}", source)?;
+        }
+        Ok(())
+    }
 }
