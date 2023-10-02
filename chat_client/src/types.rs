5b27b775d1862768802211fb6c3cedf26e8f95df
Phill <philltv.gaming@gmail.com>
2023-10-02 15:47:47 +0200

    ref: remove ClientProtocol from client

--- chat_client/src/types.rs
+++ chat_client/src/types.rs
@@ -1,27 +1,5 @@
 use std::net::SocketAddr;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub enum ServerProtocol<'a> {
-    BroadcastMessage { sender: &'a str, content: &'a str },
-    AuthenticateToken { token: &'a str },
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub enum ClientProtocol<'a> {
-    SendMessage {
-        hwid: &'a str,
-        content: &'a str,
-    },
-    ChangeUsername {
-        hwid: &'a str,
-        new_username: &'a str,
-    },
-    RequestAuthentication {
-        hwid: &'a str,
-        name: &'a str,
-    },
-}
-
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Config {
     pub endpoint: SocketAddr,
