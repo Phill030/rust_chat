c6b8e9e9291430d456c72fa9cd366f8820c6a9c7
Phill <philltv.gaming@gmail.com>
2023-10-04 01:05:35 +0200

    docs: add comments

--- chat_shared/src/protocols/client.rs
+++ chat_shared/src/protocols/client.rs
@@ -1,5 +1,3 @@
-use std::io::Cursor;
-
 use super::server::{AuthenticateToken, BroadcastMessage, ServerMessageType};
 use crate::{
     error::{DeserializerError, SerializerError},
@@ -7,6 +5,7 @@ use crate::{
     utils::{prepare_inner_cursor, read_string_from_buffer, time_in_seconds},
 };
 use async_trait::async_trait;
+use std::io::Cursor;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[derive(PartialEq, Debug)]
@@ -244,3 +243,5 @@ impl Deserializer for AuthenticateToken {
         }));
     }
 }
+
+// TODO: Make custom implementation for adding header, content, etc.
