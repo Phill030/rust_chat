a8939910d62da7f32455f44311dd97405a8b16a8
Phill <50775241+Phill030@users.noreply.github.com>
2023-10-05 09:51:23 +0200

    feat(test): compare timestamp with 2 minute invalidation

--- chat_shared/src/utils.rs
+++ chat_shared/src/utils.rs
@@ -1,9 +1,13 @@
 use crate::error::DeserializerError;
-use std::{io::Cursor, time::SystemTime};
+use std::{
+    io::Cursor,
+    time::{SystemTime, UNIX_EPOCH},
+};
 use tokio::io::AsyncReadExt;
 
+#[must_use]
 pub fn time_in_seconds() -> u64 {
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => n.as_secs(),
         Err(_) => panic!("SystemTime before UNIX_EPOCH!"),
     }
@@ -28,4 +32,22 @@ pub async fn prepare_inner_cursor(cursor: &mut Cursor<&[u8]>) -> std::io::Result
     cursor.read(&mut buffer).await?;
 
     Ok(Cursor::new(buffer))
+}
+
+const VALID_RANGE: i64 = 60; // 1 Minute
+#[must_use]
+pub fn cmp_timestamp(time: u64) -> bool {
+    let now = match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        Err(_) => panic!("SystemTime before UNIX_EPOCH!"),
+    };
+
+    let now_i32 = i64::try_from(now).unwrap();
+    let time_i32 = i64::try_from(time).unwrap();
+
+    if time_i32 - now_i32 > VALID_RANGE {
+        return false;
+    }
+
+    true
 }
