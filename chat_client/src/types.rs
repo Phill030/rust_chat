2e3a4b54ee4318604a5c786fd5d4ca21cd561b28
Phill <50775241+Phill030@users.noreply.github.com>
2023-10-05 07:56:20 +0200

    feat: add connection timeout

--- chat_client/src/types.rs
+++ chat_client/src/types.rs
@@ -1,10 +1,11 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Config {
     pub endpoint: SocketAddr,
     pub buffer_size: usize,
     pub name: String,
+    pub timeout: Duration,
 }
 
 impl Default for Config {
@@ -13,6 +14,7 @@ impl Default for Config {
             endpoint: "127.0.0.1:7878".parse().unwrap(),
             buffer_size: 2048,
             name: format!("User{}", rand::prelude::random::<i16>()),
+            timeout: Duration::from_secs(10),
         }
     }
 }
