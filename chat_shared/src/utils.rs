9ea77312577af14d894b38ddf1cda3ec6541a7b4
Phill <philltv.gaming@gmail.com>
2023-10-26 01:08:57 +0200

    feat: start to add custom macros to read

--- chat_shared/src/utils.rs
+++ chat_shared/src/utils.rs
@@ -1,9 +1,43 @@
 use crate::error::DeserializerError;
-use std::{
-    io::Cursor,
-    time::{SystemTime, UNIX_EPOCH},
-};
+use std::io::Cursor;
 use tokio::io::AsyncReadExt;
+
+macro_rules! read_type {
+    ($cursor:expr, $ty:ty) => {{
+        use std::io::Read;
+        let mut buf = [0; std::mem::size_of::<$ty>()];
+        $cursor
+            .read_exact(&mut buf)
+            .await
+            .expect("Failed to read data");
+        <$ty>::from_le_bytes(buf)
+    }};
+}
+
+macro_rules! read {
+    ($cursor:expr, String, $length:expr) => {{
+        use std::io::Read;
+        let mut buf = vec![0; $length];
+        $cursor
+            .read_exact(&mut buf)
+            .await
+            .expect("Failed to read data");
+        String::from_utf8(buf).expect("Failed to convert to String")
+    }};
+
+    ($cursor:expr, BigString) => {{
+        use std::io::Read;
+        let str_len = read_type!($cursor, u32);
+
+        // Read string using length
+        let mut buf = vec![0; str_len as usize];
+        $cursor
+            .read_exact(&mut buf)
+            .await
+            .expect("Failed to read data");
+        String::from_utf8(buf).expect("Failed to convert to String")
+    }};
+}
 
 pub async fn read_string_from_buffer(
     buffer: &mut Cursor<Vec<u8>>,
@@ -19,8 +53,8 @@ pub async fn read_string_from_buffer(
 }
 
 pub async fn prepare_inner_cursor(cursor: &mut Cursor<&[u8]>) -> std::io::Result<Cursor<Vec<u8>>> {
-    let msg_length = cursor.read_u32().await?;
-    let mut buffer = vec![0u8; msg_length as usize];
+    let inner_length = cursor.read_u32().await?;
+    let mut buffer = vec![0u8; inner_length as usize];
     cursor.read(&mut buffer).await?;
 
     Ok(Cursor::new(buffer))
