20f782d0d36aa663a0d349b0697f1d0583d95578
Phill <philltv.gaming@gmail.com>
2023-10-25 15:45:48 +0200

    feat: add tests in main

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,3 +1,9 @@
+extern crate chat_macro;
+
+use chat_shared::{
+    protocols::client::ChatMessage,
+    types::{Deserialize, Serialize},
+};
 use config::config::ConfigManager;
 use server::Server;
 
@@ -10,6 +16,17 @@ pub mod utils;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let x = ChatMessage {
+        hwid: "BBBB".to_string(),
+        content: "AAAA".to_string(),
+    }
+    .serialize()
+    .await
+    .unwrap();
+
+    log::error!("{:?}", x);
+    log::error!("{:?}", ChatMessage::deserialize(&x).await.unwrap());
 
     // let db = Surreal::new::<Mem>(()).await.unwrap();
     // db.use_ns("chat").use_db("clients").await.unwrap();
