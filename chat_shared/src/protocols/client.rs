2952fd580defd98158e7657acef0b95769b3a192
Phill <philltv.gaming@gmail.com>
2023-10-25 15:43:38 +0200

    ref: use derive macros to (De)serialize (FINALLY!!)

--- chat_shared/src/protocols/client.rs
+++ chat_shared/src/protocols/client.rs
@@ -1,8 +1,7 @@
-use super::server::{AuthenticateToken, BroadcastMessage, ServerMessageType};
 use crate::{
     error::{DeserializerError, SerializerError},
     types::{Deserialize, Serialize},
-    utils::{prepare_inner_cursor, read_string_from_buffer, time_in_seconds},
+    utils::{prepare_inner_cursor, read_string_from_buffer},
 };
 use async_trait::async_trait;
 use std::io::Cursor;
@@ -27,221 +26,24 @@ impl From<u8> for ClientMessageType {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, chat_macro::Serialize, chat_macro::Deserialize)]
+#[Belonging(ClientMessageType)]
 pub struct ChatMessage {
     pub hwid: String,
     pub content: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, chat_macro::Serialize, chat_macro::Deserialize)]
+#[Belonging(ClientMessageType)]
+
 pub struct ChangeUsername {
     pub hwid: String,
     pub new_username: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, chat_macro::Serialize, chat_macro::Deserialize)]
+#[Belonging(ClientMessageType)]
 pub struct RequestAuthentication {
     pub hwid: String,
     pub name: String,
 }
-
-//\\//\\//\\//\\//\\//\\//\\//\\
-// CLIENT -> SERVER Impls (Serialize)
-//\\//\\//\\//\\//\\//\\//\\//\\
-
-#[async_trait]
-impl Serialize for ChatMessage {
-    async fn serialize(&self) -> Result<Vec<u8>, SerializerError> {
-        let mut buffer: Vec<u8> = Vec::new();
-
-        // MessageType
-        buffer
-            .write_u8(ClientMessageType::ChatMessage as u8)
-            .await?;
-
-        // CurrentTime
-        buffer.write_u64(time_in_seconds()).await?;
-
-        let mut content_buffer = Vec::new();
-        // HWID
-        content_buffer
-            .write_u32(u32::try_from(self.hwid.as_bytes().len())?)
-            .await?;
-        content_buffer.extend(self.hwid.as_bytes());
-        //Content
-        content_buffer
-            .write_u32(u32::try_from(self.content.as_bytes().len())?)
-            .await?;
-        content_buffer.extend(self.content.as_bytes());
-
-        // Information
-        let checksum = crc32fast::hash(&content_buffer[..]);
-        buffer.write_u32(checksum).await?;
-
-        // Append content_buffer length to main buffer after everything is written
-        buffer
-            .write_u32(u32::try_from(content_buffer.len())?)
-            .await?;
-        buffer.append(&mut content_buffer);
-
-        return Ok(buffer);
-    }
-}
-
-#[async_trait]
-impl Serialize for ChangeUsername {
-    async fn serialize(&self) -> Result<Vec<u8>, SerializerError> {
-        let mut buffer: Vec<u8> = Vec::new();
-
-        // MessageType
-        buffer
-            .write_u8(ClientMessageType::ChangeUsername as u8)
-            .await?;
-
-        // CurrentTime
-        buffer.write_u64(time_in_seconds()).await?;
-
-        let mut content_buffer = Vec::new();
-        // HWID
-        content_buffer
-            .write_u32(u32::try_from(self.hwid.as_bytes().len())?)
-            .await?;
-        content_buffer.extend(self.hwid.as_bytes());
-        //Content
-        content_buffer
-            .write_u32(u32::try_from(self.new_username.as_bytes().len())?)
-            .await?;
-        content_buffer.extend(self.new_username.as_bytes());
-
-        // Information
-        let checksum = crc32fast::hash(&content_buffer[..]);
-        buffer.write_u32(checksum).await?;
-
-        // Append content_buffer length to main buffer after everything is written
-        buffer
-            .write_u32(u32::try_from(content_buffer.len())?)
-            .await?;
-        buffer.append(&mut content_buffer);
-
-        return Ok(buffer);
-    }
-}
-
-#[async_trait]
-impl Serialize for RequestAuthentication {
-    async fn serialize(&self) -> Result<Vec<u8>, SerializerError> {
-        let mut buffer: Vec<u8> = Vec::new();
-
-        // MessageType
-        buffer
-            .write_u8(ClientMessageType::RequestAuthentication as u8)
-            .await?;
-
-        // CurrentTime
-        buffer.write_u64(time_in_seconds()).await?;
-
-        let mut content_buffer = Vec::new();
-        // HWID
-        content_buffer
-            .write_u32(u32::try_from(self.hwid.as_bytes().len())?)
-            .await?;
-        content_buffer.extend(self.hwid.as_bytes());
-        //Content
-        content_buffer
-            .write_u32(u32::try_from(self.name.as_bytes().len())?)
-            .await?;
-        content_buffer.extend(self.name.as_bytes());
-
-        // Information
-        let checksum = crc32fast::hash(&content_buffer[..]);
-        buffer.write_u32(checksum).await?;
-
-        // Append content_buffer length to main buffer after everything is written
-        buffer
-            .write_u32(u32::try_from(content_buffer.len())?)
-            .await?;
-        buffer.append(&mut content_buffer);
-
-        return Ok(buffer);
-    }
-}
-
-//\\//\\//\\//\\//\\//\\//\\//\\
-// CLIENT <- SERVER Impls (Deserialize)
-//\\//\\//\\//\\//\\//\\//\\//\\
-
-#[async_trait]
-impl Deserialize for BroadcastMessage {
-    async fn deserialize<'a>(data: &'a [u8]) -> Result<Self, DeserializerError>
-    where
-        Self: Sized,
-    {
-        if data.is_empty() {
-            return Err(DeserializerError::InvalidBufferLength);
-        }
-        let mut data = Cursor::new(data);
-
-        let msg_type = data.read_u8().await?;
-        let message_type = ServerMessageType::from(msg_type);
-        if message_type != ServerMessageType::BroadcastMessage {
-            return Err(DeserializerError::InvalidMessageType);
-        }
-
-        // Invalid message (Slow response?)
-        let timestamp = data.read_u64().await?;
-        // TODO: Check if time is in range of 2 minutes
-        let checksum = data.read_u32().await?;
-        // TODO: Compare checksums
-
-        let mut inner_cursor = prepare_inner_cursor(&mut data).await?;
-        let username = read_string_from_buffer(&mut inner_cursor).await?;
-        let content = read_string_from_buffer(&mut inner_cursor).await?;
-
-        if username.is_none() || content.is_none() {
-            return Err(DeserializerError::InvalidData);
-        }
-
-        return Ok(Self {
-            username: username.unwrap(),
-            content: content.unwrap(),
-        });
-    }
-}
-
-#[async_trait]
-impl Deserialize for AuthenticateToken {
-    async fn deserialize<'a>(data: &'a [u8]) -> Result<Self, DeserializerError>
-    where
-        Self: Sized,
-    {
-        if data.is_empty() {
-            return Err(DeserializerError::InvalidBufferLength);
-        }
-        let mut data = Cursor::new(data);
-
-        let msg_type = data.read_u8().await?;
-        let message_type = ServerMessageType::from(msg_type);
-        if message_type != ServerMessageType::AuthenticateToken {
-            return Err(DeserializerError::InvalidMessageType);
-        }
-
-        // Invalid message (Slow response?)
-        let timestamp = data.read_u64().await?;
-        // TODO: Check if time is in range of 2 minutes
-        let checksum = data.read_u32().await?;
-        // TODO: Compare checksums
-
-        let mut inner_cursor = prepare_inner_cursor(&mut data).await?;
-        let token = read_string_from_buffer(&mut inner_cursor).await?;
-
-        if token.is_none() {
-            return Err(DeserializerError::InvalidData);
-        }
-
-        return Ok(Self {
-            token: token.unwrap(),
-        });
-    }
-}
-
-// TODO: Make custom implementation for adding header, content, etc.
