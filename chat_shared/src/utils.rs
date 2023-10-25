01360cd0f9e1054be1de70dcf53d5b5a650637cb
Phill <philltv.gaming@gmail.com>
2023-10-25 14:53:56 +0200

    ref: remove timestamps in messages

--- chat_shared/src/utils.rs
+++ chat_shared/src/utils.rs
@@ -5,14 +5,6 @@ use std::{
 };
 use tokio::io::AsyncReadExt;
 
-#[must_use]
-pub fn time_in_seconds() -> u64 {
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => n.as_secs(),
-        Err(_) => panic!("SystemTime before UNIX_EPOCH!"),
-    }
-}
-
 pub async fn read_string_from_buffer(
     buffer: &mut Cursor<Vec<u8>>,
 ) -> Result<Option<String>, DeserializerError> {
@@ -32,22 +24,4 @@ pub async fn prepare_inner_cursor(cursor: &mut Cursor<&[u8]>) -> std::io::Result
     cursor.read(&mut buffer).await?;
 
     Ok(Cursor::new(buffer))
-}
-
-const VALID_RANGE: i64 = 60; // 1 Minute
-#[must_use]
-pub fn cmp_timestamp(time: u64) -> bool {
-    let now = match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => n.as_secs(),
-        Err(_) => panic!("SystemTime before UNIX_EPOCH!"),
-    };
-
-    let now_i32 = i64::try_from(now).unwrap();
-    let time_i32 = i64::try_from(time).unwrap();
-
-    if time_i32 - now_i32 > VALID_RANGE {
-        return false;
-    }
-
-    true
 }
